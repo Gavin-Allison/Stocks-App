42f4921b864534b10ec7c73df0e626e081b31be7
Gavin Allison <gavinallison007@gmail.com>
2026-04-24 18:05:06 -0700

    Added temporary list barebones for left column

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,26 @@ export default function App() {
   const layout: React.CSSProperties & { [key: string]: any } = {
     "--layout-width": "1600px",
   }
+
+  const temp_list = [{
+    id: 0,
+    stock: "AAPL",
+  }, {
+    id: 1,
+    stock: "GOOG",
+  }, {
+    id: 2,
+    stock: "AMZN",
+  }, {
+    id: 3,
+    stock: "MSFT",
+  }]  
+
+  const temp_list_items = temp_list.map((item) => (
+    <div key={item.id} className="p-2 border-b border-gray-300">
+      {item.stock}
+    </div>
+  ))
    
   return (
     <div className="flex h-screen w-full flex-col" style={layout}>
@@ -23,6 +43,9 @@ export default function App() {
           {/* Left Column*/}
           <section className="flex-1 border-r border-black">
             <h1>Left Column</h1>
+            <div>
+              {temp_list_items}
+            </div>
           </section>
 
           {/* Right Column */}
