1711dd19579ed83af22cc7b5fd846f06ffa206e1
Gavin Allison <gavinallison007@gmail.com>
2026-04-24 17:51:04 -0700

    Removed magic numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 export default function App() {
+  const layout: React.CSSProperties & { [key: string]: any } = {
+    "--layout-width": "1600px",
+  }
+   
   return (
-    <div className="flex h-screen w-full flex-col">
+    <div className="flex h-screen w-full flex-col" style={layout}>
+
       <header className="h-16 w-full flex-none border-b border-black">
-        <div className="max-w-[1600px] h-full flex mx-auto border-l border-r border-black">
+        <div className="max-w-[var(--layout-width)] h-full flex mx-auto border-x border-black">
           <h1>Top Bar</h1>
         </div>
       </header>
 
       <div className="flex flex-1 justify-center @container">
-        <main className="max-w-[1600px] w-full flex mx-auto">
+        <main className="max-w-[var(--layout-width)] w-full flex mx-auto">
 
           {/* Sidebar */}
-          <aside className="w-16 @[1300px]:w-64 flex-none border-l border-r border-black">
+          <aside className="w-16 @7xl:w-64 flex-none border-x border-black">
             <h1>Sidebar</h1>
           </aside>
           
@@ -21,7 +26,7 @@ export default function App() {
           </section>
 
           {/* Right Column */}
-          <section className="w-0 @[800px]:w-64 @[1100px]:w-128 flex-none border-r border-black">
+          <section className="w-0 @4xl:w-64 @6xl:w-128 flex-none border-r border-black">
             <h1>Right Column</h1>
           </section>
 
