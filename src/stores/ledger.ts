a06485c6c2df81c9e25e298ade12fc4819f410dc
Gavin-Allison <gavinallison007@gmail.com>
2026-05-06 17:29:39 -0600

    Added barebones ledger output

--- src/stores/ledger.ts
+++ src/stores/ledger.ts
@@ -3,12 +3,24 @@ import { type Transaction } from "../types/transaction"
 export interface LedgerEntry {
     transaction: Transaction;
     currentCash: number;
-    currentAssets: number;
+    currentAssets: Record<string, number>;
     error?: string
 }
 
-export const validateLedger = (): void => {
-    // TODO: function that performs and validates all transactions 
+export const validateLedger = (transactions: Transaction[]): LedgerEntry[] => {
+    // Perform and validate all transactions
+
+    let cash = 0;
+    const assets: Record<string, number> = {};
+    let errorMessage: string | undefined;
+    return transactions.map((t) => {
+        return {
+            transaction: t,
+            currentCash: cash,
+            currentAssets: assets,
+            error: errorMessage
+        };
+    });
 }
     
     
