692036be8ed5565b7f8a3cb217370fc8d6bf8aac
Gavin-Allison <gavinallison007@gmail.com>
2026-05-14 21:59:02 -0600

    seperated trades fom cash in the input area and labeled input fields

--- src/components/report/transactions.tsx
+++ src/components/report/transactions.tsx
@@ -10,9 +10,12 @@ export const Transactions = () => {
     const { stocks, transactions, priceData, date, selectedStock, setDate, setSelectedStock, addTransaction, removeTransaction, getStockPriceAtDate } = useAppStore()
 
     const [numStocks, setNumStocks] = useState<number>(1);
+    const [percentOfCash, setPercentOfCash] = useState<number>(20);
     const [tradeFee, setTradeFee] = useState<number>(10);
     const [cashFee, setCashFee] = useState<number>(10);
     const [draftTransactions, setDraftTransactions] = useState<Transaction[]>([]);
+    const [tradeOrCash, setTradeOrCash] = useState<"TRADE" | "CASH">("TRADE");
+    const [fixedOrDynamic, setFixedOrDynamic] = useState<"FIXED" | "DYNAMIC">("FIXED");
 
     const currentPrice = getStockPriceAtDate(selectedStock, date) || 0;
 
@@ -39,6 +42,26 @@ export const Transactions = () => {
         const combinedTransactions = combineSortedTransactions(transactions, draftTransactions);
         return validateLedger(combinedTransactions, priceData);
     }, [transactions, draftTransactions, priceData]);
+
+    const handleAddTransaction = (details: any) => {
+        const transaction: Transaction = {
+            ...details,
+            id: crypto.randomUUID(),
+            date: date,
+        }
+        setDraftTransactions((prev) => {
+            const insertionIndex = prev.findIndex((t) => t.date > transaction.date);
+            if (insertionIndex === -1) {
+                return [...prev, transaction];
+            }
+            return [...prev.slice(0, insertionIndex), transaction, ...prev.slice(insertionIndex)];
+        });
+    }
+
+    const handleSubmitTransactions = () => {
+        draftTransactions.forEach((transaction) => addTransaction(transaction));
+        setDraftTransactions([]);
+    }
 
     function formatTransaction(transaction: Transaction): string {
         switch (transaction.type) {
@@ -84,55 +107,17 @@ export const Transactions = () => {
         );
     });
 
-    const errorOutput = previewLedger.find(e => e.error)?.errorMessage || "No errors";
-
-    const handleAddTransaction = (details: any) => {
-        const transaction: Transaction = {
-            ...details,
-            id: crypto.randomUUID(),
-            date: date,
-        }
-        setDraftTransactions((prev) => {
-            const insertionIndex = prev.findIndex((t) => t.date > transaction.date);
-            if (insertionIndex === -1) {
-                return [...prev, transaction];
-            }
-            return [...prev.slice(0, insertionIndex), transaction, ...prev.slice(insertionIndex)];
-        });
-    }
-
-    const handleSubmitTransactions = () => {
-        draftTransactions.forEach((transaction) => addTransaction(transaction));
-        setDraftTransactions([]);
-    }
-
-    return (
-        <div className="flex flex-col w-full h-full p-4">
-
-            {/* Header with date picker */}
-            <div className="flex items-center justify-between mb-4"> 
-                <h1 className="text-xl font-bold mb-4">Transactions</h1>
-                <StockDatePicker className="w-38" date={date} onDateChange={setDate} />
-            </div>
-            
-            {/* Transaction buttons and inputs */}
-            <div className="flex flex-wrap gap-2 mb-4">
-                <button onClick={() => handleAddTransaction({ type: "FBUY", ticker: selectedStock, amount: numStocks, pricePerUnit: currentPrice, fees: tradeFee })} className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700">Buy #</button>
-                <button onClick={() => handleAddTransaction({ type: "FSELL", ticker: selectedStock, amount: numStocks, pricePerUnit: currentPrice, fees: tradeFee })} className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700">Sell #</button>
-                <button onClick={() => handleAddTransaction({ type: "DBUY", ticker: selectedStock, value: 0.2, fees: cashFee })} className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700">Buy %</button>
-                <button onClick={() => handleAddTransaction({ type: "DSELL", ticker: selectedStock, value: 0.2, fees: cashFee })} className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700">Sell %</button>
-                <button onClick={() => handleAddTransaction({ type: "DEPOSIT", amount: 100, fees: cashFee })} className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700">Deposit</button>
-                <button onClick={() => handleAddTransaction({ type: "WITHDRAWAL", amount: 100, fees: cashFee })} className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700">Withdraw</button>
-            </div>
-
-            {/* Input fields for transaction details */}
-            <div className="mb-4 w-full h-1/2 border border-gray-400 rounded bg-gray-200 flex flex-col justify-between">
-                <div className="flex gap-2 p-2">
+    const tradeInputs = (
+        <div className="flex flex-col w-full gap-2 p-2">
+            <div className="flex flex-row w-full items-center justify-between text-gray-700"> 
+                
+                <div className="flex flex-row">
+                    <h1>Select Stock: </h1>
                     {/* Dropdown to select stock for transaction */}
                     <select 
                         value={selectedStock} 
                         onChange={(e) => setSelectedStock(e.target.value)} 
-                        className="bg-white border border-gray-400 rounded"
+                        className="w-26 bg-white border border-gray-400 rounded ml-2"
                     >
                         {stocks.map((stock) => (
                             <option key={stock.ticker} value={stock.ticker}>
@@ -140,36 +125,129 @@ export const Transactions = () => {
                             </option>
                         ))}
                     </select>
-
-                    {/* Input field for number of stocks */}
-                    <input
-                        type="number"
-                        value={numStocks}
-                        onChange={(e) => setNumStocks(Number(e.target.value))}
-                        placeholder="Number of Stocks"
-                        className="w-24 bg-white border border-gray-400 rounded"
-                    />
-
-                    {/* Input fields for trade fee and cash fee */}
-                    <input
-                        type="number"
-                        value={tradeFee}
-                        onChange={(e) => setTradeFee(Number(e.target.value))}
-                        placeholder="Trade Fee"
-                        className="w-24 bg-white border border-gray-400 rounded"
-                    />
-                    <input
-                        type="number"
-                        value={cashFee}
-                        onChange={(e) => setCashFee(Number(e.target.value))}
-                        placeholder="Cash Fee"
-                        className="w-24 bg-white border border-gray-400 rounded"
-                    />
                 </div>
 
+                {/* Input field for number of stocks/percentage of cash */}
+                {fixedOrDynamic === "FIXED" && (
+                    <div className="flex text-gray-700">
+                        <h1>Number of Stocks: </h1>
+                        <input
+                            type="number"
+                            value={numStocks}
+                            onChange={(e) => setNumStocks(Number(e.target.value))}
+                            className="w-16 bg-white border border-gray-400 rounded ml-2"
+                        />
+                        <button onClick={() => setFixedOrDynamic("DYNAMIC")} className="flex w-6 ml-2 items-center justify-center bg-gray-300 rounded hover:bg-gray-400">#</button>
+                    </div>
+                    
+                )}
+                {fixedOrDynamic === "DYNAMIC" && (
+                    <div className="flex text-gray-700">
+                        <h1>Percentage of Cash: </h1>
+                        <input
+                            type="number"
+                            value={percentOfCash}
+                            onChange={(e) => setPercentOfCash(Number(e.target.value))}
+                            className="w-16 bg-white border border-gray-400 rounded ml-2"
+                        />
+                        <button onClick={() => setFixedOrDynamic("FIXED")} className="flex w-6 ml-2 justify-center bg-gray-300 rounded hover:bg-gray-400">%</button>
+                    </div>
+                )}
+            </div>
+
+            {/* Input field for trade fee */}
+            <div className="flex text-gray-700">
+                <h1>Trade Fee: </h1>
+                <input
+                    type="number"
+                    value={tradeFee}
+                    onChange={(e) => setTradeFee(Number(e.target.value))}
+                    className="w-24 bg-white border border-gray-400 rounded ml-2"
+                />
+            </div>
+
+            {fixedOrDynamic === "FIXED" && (
+                <div className="flex gap-2">
+                    <button onClick={() => handleAddTransaction({ type: "FBUY", ticker: selectedStock, amount: numStocks, pricePerUnit: currentPrice, fees: tradeFee })} className="bg-blue-600 text-white w-16 py-1 rounded hover:bg-blue-700">Buy #</button>
+                    <button onClick={() => handleAddTransaction({ type: "FSELL", ticker: selectedStock, amount: numStocks, pricePerUnit: currentPrice, fees: tradeFee })} className="bg-blue-600 text-white w-16 py-1 rounded hover:bg-blue-700">Sell #</button>
+                </div>
+            )}
+            {fixedOrDynamic === "DYNAMIC" && (
+                <div className="flex gap-2">
+                    <button onClick={() => handleAddTransaction({ type: "DBUY", ticker: selectedStock, value: percentOfCash, fees: tradeFee })} className="bg-blue-600 text-white w-16 py-1 rounded hover:bg-blue-700">Buy %</button>
+                    <button onClick={() => handleAddTransaction({ type: "DSELL", ticker: selectedStock, value: percentOfCash, fees: cashFee })} className="bg-blue-600 text-white w-16 py-1 rounded hover:bg-blue-700">Sell %</button>
+                </div>
+            )}
+            
+        </div>
+    );
+
+    const cashInputs = (
+        <div className="flex flex-col gap-2 m-2">
+            {/* Input field for cash amount */}
+            <div className="text-gray-700">Cash Amount: 
+                <input
+                    type="number"
+                    value={numStocks}
+                    onChange={(e) => setNumStocks(Number(e.target.value))}
+                    className="w-24 bg-white border border-gray-400 rounded ml-2"
+                />
+            </div>
+
+            {/* Input field for cash fee */}
+            <div className="text-gray-700">Cash Fee: 
+                <input
+                    type="number"
+                    value={cashFee}
+                    onChange={(e) => setCashFee(Number(e.target.value))}
+                    className="w-24 bg-white border border-gray-400 rounded ml-2"
+                />
+            </div>
+
+            <div className="flex gap-2">
+                <button onClick={() => handleAddTransaction({ type: "DEPOSIT", amount: 100, fees: cashFee })} className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700">Deposit</button>
+                <button onClick={() => handleAddTransaction({ type: "WITHDRAWAL", amount: 100, fees: cashFee })} className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700">Withdraw</button>
+            </div>
+        </div>
+    );
+
+    const errorOutput = previewLedger.find(e => e.error)?.errorMessage || "No errors";
+
+
+
+    return (
+        <div className="flex flex-col w-full h-full p-4">
+
+            {/* Header with date picker */}
+            <div className="flex items-center justify-between"> 
+                <h1 className="text-xl font-bold">Transactions</h1>
+                <button onClick={() => setTradeOrCash("TRADE")} className="bg-blue-600 text-white px-5 py-1 rounded hover:bg-blue-700">Trade</button>
+                <button onClick={() => setTradeOrCash("CASH")} className="bg-blue-600 text-white px-5 py-1 rounded hover:bg-blue-700">Cash</button>
+                <StockDatePicker className="w-38" date={date} onDateChange={setDate} />
+            </div>
+            
+            {/* Transaction buttons and inputs */}
+            <div className="flex flex-wrap gap-2 mb-4">
+
+            </div>
+
+            {/* Input fields for transaction details */}
+            <div className="flex flex-col mb-4 w-full h-1/2 border border-gray-400 rounded bg-gray-200 justify-between">
+                {tradeInputs && cashInputs && (
+                    <div className="flex w-full h-full">
+                        <div className= {`w-full ${tradeOrCash === "TRADE" ? "" : "hidden"}`}>
+                            {tradeInputs}
+                        </div>
+                        <div className={`w-full ${tradeOrCash === "CASH" ? "" : "hidden"}`}>
+                            {cashInputs}
+                        </div>
+                    </div>
+                )}
+
+                {/* Current stock price and submit button */}
                 <div className="flex items-center justify-between p-2">
                     <h1 className="text-lg font-bold">
-                        Current Price: ${currentPrice.toFixed(2)}
+                        Share Price: ${currentPrice.toFixed(2)} 
                     </h1>
 
                     {/* Button to submit pending transactions */}
