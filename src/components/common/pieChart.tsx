60d6eb5eb960073fd06fa66bdd36ae29143fe94b
Gavin-Allison <gavinallison007@gmail.com>
2026-05-21 16:46:34 -0600

    Made pie chart more readable

--- src/components/common/pieChart.tsx
+++ src/components/common/pieChart.tsx
@@ -5,20 +5,25 @@ export const StockPieChart = ({
 }: { 
     assets: Record<string, number>; 
 }) => {
-    // Transform { Apple: 70, Google: 30 } into [{ id: 'Apple', value: 70, label: 'Apple' }, ...]
+    // Transform { AAPL: 70, GOOG: 30 } into [{ id: 'AAPL', value: 70, label: 'AAPL' }, ...]
     const chartData = Object.entries(assets).map(([label, value]) => ({
         id: label,
         value: value,
         label: label,
     }));
+    const totalValue = Object.values(assets).reduce((a, b) => a + b, 0);
 
-    return (
+return (
         <PieChart
             series={[
                 {
                     data: chartData,
+                    arcLabel: (item) => `${((item.value / totalValue) * 100).toFixed(0)}%`,
                 },
             ]}
+            skipAnimation
+            width={200}
+            height={200}
         />
     );
 };
