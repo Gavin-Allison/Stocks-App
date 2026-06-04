68677a5235a0f2790d28bf7d5be4d77c206f6bc7
Gavin-Allison <gavinallison007@gmail.com>
2026-06-04 14:53:36 -0600

    Added borders to buttons

--- frontend/src/styles/tokens.ts
+++ frontend/src/styles/tokens.ts
@@ -1,10 +1,10 @@
 export const theme = {
   button: {
-    primary: 'bg-blue-600 text-white hover:bg-blue-700',
-    danger: 'bg-red-100 text-red-600 hover:bg-red-200',
-    success: 'bg-green-600 text-white hover:bg-green-700',
-    muted: 'bg-gray-200 text-gray-800 hover:bg-gray-300',
-    light: 'bg-white text-gray-700 hover:bg-gray-200',
+    primary: 'bg-blue-600 text-white hover:bg-blue-700 border border-gray-400',
+    danger: 'bg-red-100 text-red-600 hover:bg-red-200 border border-gray-400',
+    success: 'bg-green-600 text-white hover:bg-green-700 border border-gray-400',
+    muted: 'bg-gray-200 text-gray-800 hover:bg-gray-300 border border-gray-400',
+    light: 'bg-white text-gray-700 hover:bg-gray-200 border border-gray-400',
   },
   panel: {
     muted: 'bg-gray-200',
